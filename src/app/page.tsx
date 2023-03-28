049ed13ba1b73e9919d1dc10914613afba1cbf23
Marco Meijer <marcomeijerm@gmail.com>
2023-03-29 00:15:50 +0200

    feat: basic 3d scene

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
-import Button from "@/components/button";
+"use client";
 import LinkButton from "@/components/linkButton";
+import Floor from "@/components/three/floor";
+import { Canvas } from "@react-three/fiber";
 
 export default function Home() {
   return (
-    <main className="h-screen bg-black p-10">
+    <main className="h-screen p-10">
+      <div className="absolute w-screen h-screen top-0 left-0 -z-10">
+        <Canvas
+          shadows
+          camera={{
+            position: [-6, 7, 7],
+          }}
+          className="bg-black"
+        >
+          <Floor />
+          <ambientLight color="white" intensity={0.3} />
+        </Canvas>
+      </div>
       <h1 className="flex text-9xl font-bold text-white items-center">
         Marco Lucas <br />
         Meijer
---
+++ src/components/three/floor.tsx
@@ -0,0 +1,3 @@
+export default function Floor() {
+  return <gridHelper args={[200, 50]} />;
+}

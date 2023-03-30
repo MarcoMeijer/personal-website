c94814b7c7f6f0ef38ec866d5a915316701763e0
Marco Meijer <marcomeijerm@gmail.com>
2023-03-30 21:44:53 +0200

    feat: hiding navbar on scroll

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,39 @@
 "use client";
+import { useEffect, useRef, useState } from "react";
 
 export interface NavbarProps {
   sections: { title: string }[];
 }
 
 export default function Navbar({ sections }: NavbarProps) {
+  const [show, setShow] = useState(true);
+  const lastScrollY = useRef(0);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const controlNavbar = () => {
+        if (typeof window !== "undefined") {
+          if (window.scrollY > lastScrollY.current) {
+            setShow(false);
+          } else {
+            setShow(true);
+          }
+
+          lastScrollY.current = window.scrollY;
+        }
+      };
+
+      window.addEventListener("scroll", controlNavbar);
+      return () => window.removeEventListener("scroll", controlNavbar);
+    }
+  }, []);
+
   return (
-    <div className="flex flex-row fixed z-40 w-screen bg-black bg-opacity-50">
+    <div
+      className={`flex flex-row fixed z-40 w-screen bg-black bg-opacity-50 transition-all duration-300 ${
+        show || "-translate-y-64"
+      }`}
+    >
       {sections.map(({ title }, i) => (
         <button
           key={i}

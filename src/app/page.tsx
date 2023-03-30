e4c4a4bfb3d7b31a91711dc12c797827e36c7c29
Marco Meijer <marcomeijerm@gmail.com>
2023-03-30 18:45:41 +0200

    feat: resume button animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,20 +16,23 @@ export default function Home() {
       <div className="flex flex-col text-8xl lg:text-10xl leading-none font-bold text-white items-begin pointer-events-none relative z-100">
         <SplitText
           text="Marco Lucas"
-          startDelay={1}
+          startDelay={0.5}
           deltaDelay={0.08}
           className="animate-move-letter fill-mode-backwards"
         />
         <div className="pl-24">
           <SplitText
             text="Meijer"
-            startDelay={2.2}
+            startDelay={1.4}
             deltaDelay={0.08}
             className="animate-move-letter fill-mode-backwards"
           />
         </div>
       </div>
-      <div className="left-40 bottom-28 lg:left-80 lg:bottom-60 absolute z-10">
+      <div
+        className="left-40 bottom-28 lg:left-80 lg:bottom-60 absolute z-10 animate-enter-up"
+        style={{ animationDelay: "2.3s", animationFillMode: "backwards" }}
+      >
         <LinkButton title="resume" href="/resume" />
       </div>
     </main>

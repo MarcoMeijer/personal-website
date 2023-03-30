e8cecaa64922cd1a8ffa9160b35df86065069eda
Marco Meijer <marcomeijerm@gmail.com>
2023-03-30 23:49:34 +0200

    fix: improved responsive design

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -44,7 +44,7 @@ export default function Section({ title, components, index }: SectionProps) {
 
   return (
     <div
-      className={`p-5 backdrop-blur-sm w-7/12 m-4 lg:m-16 bg-gray-600 bg-opacity-30 rounded-2xl ${
+      className={`p-5 backdrop-blur-sm w-12/12 md:w-7/12 m-4 lg:m-16 bg-gray-600 bg-opacity-30 rounded-2xl ${
         index % 2 === 0 ? "self-start" : "self-end"
       }`}
       id={title}
